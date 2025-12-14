ec1bc38f00568120af9c335daa64ab33fe6ad4b9
DipeshDalmia <dalmiaanshu9@gmail.com>
2025-12-14 18:58:06 +0530

    fix cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 
-const authRoutes = require("./routes/authRoutes.js");
-const sweetRoutes = require("./routes/sweetRoute.js");
+const authRoutes = require("./routes/authRoutes");
+const sweetRoutes = require("./routes/sweetRoute");
 
 const app = express();
 
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",              
-      "https://sweet-shop-green.vercel.app/"    
+      "http://localhost:5173",
+      "https://sweet-shop-green.vercel.app"
     ],
-    credentials: true,
   })
 );
 
